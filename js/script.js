5c645dff1065d801b3c9fe3177a27742d9c97c54
mascot86 <mascot86@mail.ru>
2022-02-17 13:09:17 +0300

    Add project files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('.red-form-btn').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.modal').fadeIn().show();
+	});
+	
+	$('.modal-close-btn').on('click', function () {
+		$('.modal').fadeOut().hide();
+	});
+});
